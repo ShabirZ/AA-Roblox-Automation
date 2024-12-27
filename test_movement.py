b730772a918fd687e323d692ae759256abfa6c4c
shabir-zahir <shabirzahir1202@gmail.com>
2024-12-27 17:27:52 -0500

    test script

---
+++ test_movement.py
@@ -0,0 +1,25 @@
+from pyautogui import *
+import autoit
+import time
+
+# Move the mouse to x=100, y=200 and click
+time.sleep(2)
+def move_to(x,y):
+    autoit.mouse_move(x, y)
+
+def press_key(key):
+    autoit.send(key)
+
+def click(key):
+    autoit.mouse_click(key)
+
+def rotate(x):
+    # 3 = 360
+    # 1.5 = 180
+    # .75  = 90
+    
+    autoit.send("{RIGHT Down}")
+    time.sleep(x)
+    autoit.send("{RIGHT Up}")
+time.sleep(1)
+rotate(.75)
